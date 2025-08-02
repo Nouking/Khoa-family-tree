953599ef0af129ae50482106cb29849a87f9aa00
Khoa Hoang Ngoc Anh <squall.lionheart.92@gmail.com>
2025-08-02 10:43:56 +0700

    Update task tracking and completed tasks

--- family-tree/app/components/FamilyTreeCanvas.tsx
+++ family-tree/app/components/FamilyTreeCanvas.tsx
@@ -9,6 +9,12 @@ interface FamilyTreeCanvasProps {
 const FamilyTreeCanvas: React.FC<FamilyTreeCanvasProps> = ({ familyTree }) => {
   return (
     <div className="relative w-full h-screen bg-gray-100">
+      {/* Connections Layer */}
+      <svg className="absolute inset-0 w-full h-full pointer-events-none">
+        {/* Connection lines will be rendered here */}
+      </svg>
+
+      {/* Members Layer */}
       {familyTree.members.map((member) => (
         <MemberCard key={member.id} member={member} />
       ))}
